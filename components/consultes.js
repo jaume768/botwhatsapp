96f1ab24e74efd2dbecf17a6328b8b70f40b6f1e
jaume768 <jaumefernandezsunyer10@gmail.com>
2023-02-14 15:16:03 +0100

    push

--- components/consultes.js
+++ components/consultes.js
@@ -51,11 +51,13 @@ async function insertarInfromacion(nombre,informacion){
     const idPersona = await connection.promise().query(id_persona, [nombre]);
 
     let persona;
-    if (idPersona[0][0].id_persona == undefined || idPersona[0][0].id_persona == null){
-        persona = idPersona[0].id_persona
-    } else {
+    if(idPersona[0].id_persona == undefined){
         persona = idPersona[0][0].id_persona
+    } else {
+        persona = idPersona[0].id_persona
     }
+    console.log(persona)
+
     const [idInfo] = await connection.promise().query(id_info);
     const [insertInfoPersona] = await connection.promise().execute(insertInfo_Persona, [persona,idInfo[0].id]);
 
