2236309ca4657caefc4ad8d52fe04e655e0511d7
jaume768 <jaumefernandezsunyer10@gmail.com>
2023-02-15 09:19:07 +0100

    push

--- components/consultes.js
+++ components/consultes.js
@@ -90,8 +90,8 @@ async function informacion_persona(persona,nombre) {
 
 async function actualizarInfoEspecifica(nombre,dato,informacion){
     try {
-        let update = 'UPDATE Personas SET ? = ? WHERE nombre = ?;'
-        const [results] = await connection.promise().execute(update,[dato, informacion,nombre]);
+        let update = 'UPDATE Personas SET ciudad = ? WHERE nombre = ?;'
+        const [results] = await connection.promise().execute(update,[informacion,nombre]);
       } catch (error) {
         console.log(error);
       }
